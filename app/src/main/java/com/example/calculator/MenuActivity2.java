4515cb3b4957361151bed31414d9945cb5b88399
Zhainaqova <zhainakova211@gmail.com>
2020-11-15 21:04:33 +0600

    'lesson3'

--- app/src/main/java/com/example/calculator/MenuActivity2.java
+++ app/src/main/java/com/example/calculator/MenuActivity2.java
@@ -2,15 +2,27 @@ package com.example.calculator;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public
-class MenuActivity2 extends AppCompatActivity {
+class MenuActivity2 extends AppCompatActivity implements View.OnClickListener {
+    Button btnBack;
 
     @Override
     protected
     void onCreate(Bundle savedInstanceState) {
         super.onCreate ( savedInstanceState );
         setContentView ( R.layout.activity_menu2 );
+        btnBack = findViewById ( R.id.menu1 );
+        btnBack = onClick ( this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        Intent menu1 == new Intent ( this , MenuActivity.class );
+        startActivity ( menu1 );
     }
 }
